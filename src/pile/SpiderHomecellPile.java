bc38b3b1e8e6ee4c2961b87dba03bd715c2d0f6d
Daniel <Daniel@10.84.46.189>
2018-09-28 14:57:26 -0400

    homecell

---
+++ src/pile/SpiderHomecellPile.java
@@ -0,0 +1,20 @@
+package pile;
+
+import Deck.Card;
+
+public class SpiderHomecellPile extends Pile {
+	public void initialSetup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean add(Card c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean remove() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
