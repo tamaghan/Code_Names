58ddfcad5cd7cd873d0669692aa44ee715cefea0
Daniel <Daniel@10.84.46.189>
2018-09-28 15:05:14 -0400

    Made comments

--- src/Deck/Card.java
+++ src/Deck/Card.java
@@ -1,27 +1,36 @@
 package Deck;
 
 public class Card {
+	// Creates an int and string instance
 	private int rank;
 	private String suit;
 	
+	// Constructor for this class, two params rank, suit
 	public Card (int rank, String suit) {
+		// Sets the instances equal to params
 		this.rank = rank;
 		this.suit = suit;
 	}
 	
+	// Sets rank, empty
 	public void setRank() {
 	}
+	
+	// Sets suit, empty
 	public void setSuit() {
 	}
 	
+	// Returns the card's rank
 	public int getRank() {
 		return this.rank;
 	}
 	
+	// Returns the card's suit
 	public String getSuit() {
 		return this.suit;
 	}
 	
+	// Used for testing, ignore
 	public void testReturn() {
 		System.out.println("My rank is " + rank + ". My suit is " + suit);
 	}
