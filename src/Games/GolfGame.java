eccdda897847e878c9d93f6c6b3df49ee8d00175
Daniel <Daniel@10.84.46.189>
2018-09-28 11:51:12 -0400

    golf

---
+++ src/Games/GolfGame.java
@@ -0,0 +1,20 @@
+package Games;
+
+import pile.GolfTP;
+
+/*Defines class representing a Golf game. 
+ * Each game instance has 7 tableau piles, 1 homecell pile, and 1 stock pile [4 points]
+ */
+
+public class GolfGame {
+	private GolfTP tp1;
+	private GolfTP tp2;
+	private GolfTP tp3;
+	private GolfTP tp4;
+	private GolfTP tp5;
+	private GolfTP tp6;
+	private GolfTP tp7;
+	private GolfHome home;
+	private GolfStock stock;
+	
+}
--- src/pile/GolfTP.java
+++ src/pile/GolfTP.java
@@ -2,4 +2,5 @@ package pile;
 
 public class GolfTP {
 
+	
 }
