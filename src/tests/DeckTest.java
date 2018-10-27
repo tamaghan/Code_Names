8a5a4ae3a95875ebf1bcdac0ec477d90ced470d2
HP <HP@192.168.0.9>
2018-10-26 22:06:47 -0400

    Edits made to the Deck tests

--- src/tests/DeckTest.java
+++ src/tests/DeckTest.java
@@ -16,40 +16,6 @@ public class DeckTest {
 		assertEquals(2,deck.getCardDeck()[1].getRank());
 	}
 	
-	@Test
-	public void testShuffle() {
-		Deck deck = new Deck();
-		deck.shuffle();
-		assertEquals(52,deck.getDeckSize());
-		assertEquals(2,deck.getCardDeck()[1].getRank());
-	}
-	
-	@Test 
-	public void testCut() {
-		Deck deck = new Deck();
-		int cut = 13+ (int)(Math.random()*25);
-		int cut2 = 52-cut;
-		Card[] firstHalf = new Card[cut];
-		for(int i=0; i<cut; i++) {
-			firstHalf[i]=deck.getCardDeck()[i];
-			deck.getCardDeck()[i]=null;
-		}
-		Card[] secondHalf = new Card[cut2];
-		for(int i=cut; i<52; i++) {
-			int j = 0;
-			secondHalf[j+=1]=deck.getCardDeck()[i];
-			deck.getCardDeck()[i]=null;
-		}
-		assertEquals(cut,firstHalf.length); 
-		assertEquals(cut2,secondHalf.length);
-		assertEquals(2,firstHalf[1].getRank());
-		//assertEquals(2,firstHalf[cut-1].getRank());
-		
-		assertEquals(2,secondHalf[cut2-1].getRank());
-		//assertEquals(2,secondHalf[1].getRank());
 
-		//assertEquals(cut, cut2);
-		//assertEquals(cut,deck.cutDeck());
-	}
 	
 }
