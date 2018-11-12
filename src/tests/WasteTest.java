31e4793810d7752398a33fa35a5effdb28b27a43
tbjackso <36286298+tbjackso@users.noreply.github.com>
2018-11-12 17:54:43 -0500

    Update WasteTest.java

--- src/tests/WasteTest.java
+++ src/tests/WasteTest.java
@@ -1,1 +1,24 @@
+package tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import Deck.Card;
+import Deck.Deck;
+import Games.GolfGame;
+import pile.GolfTP;
+import pile.Stock;
+
+public class WasteTest {
+  
+  @Test
+  public void testInitialWaste() 
+  {
+    FortyThieves tg = new FortyThieves();
+    assertEquals(tg.getWaste().size(), 0);
+    
+  }
+}
